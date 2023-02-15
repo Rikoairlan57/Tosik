0b0519d568491d53b0424465a116fc57d9a5865a
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-02-15 10:58:50 +0700

    fix

--- lib/common/styles.dart
+++ lib/common/styles.dart
@@ -7,65 +7,65 @@ const Color primaryColor = Color(0xFFFFFFFF);
 const Color secondaryColor = Color(0xFF1BAC4B);
 
 final TextTheme myTextTheme = TextTheme(
-  headline1: GoogleFonts.urbanist(
+  headline1: GoogleFonts.poppins(
     fontSize: 92,
     fontWeight: FontWeight.w300,
     letterSpacing: -1.5,
   ),
-  headline2: GoogleFonts.urbanist(
+  headline2: GoogleFonts.poppins(
     fontSize: 57,
     fontWeight: FontWeight.w300,
     letterSpacing: -0.5,
   ),
-  headline3: GoogleFonts.urbanist(
+  headline3: GoogleFonts.poppins(
     fontSize: 46,
     fontWeight: FontWeight.w400,
   ),
-  headline4: GoogleFonts.urbanist(
+  headline4: GoogleFonts.poppins(
     fontSize: 32,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.25,
   ),
-  headline5: GoogleFonts.urbanist(
+  headline5: GoogleFonts.poppins(
     fontSize: 23,
     fontWeight: FontWeight.w400,
   ),
-  headline6: GoogleFonts.urbanist(
+  headline6: GoogleFonts.poppins(
     fontSize: 19,
     fontWeight: FontWeight.w500,
     letterSpacing: 0.15,
   ),
-  subtitle1: GoogleFonts.urbanist(
+  subtitle1: GoogleFonts.poppins(
     fontSize: 15,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.15,
   ),
-  subtitle2: GoogleFonts.urbanist(
+  subtitle2: GoogleFonts.poppins(
     fontSize: 13,
     fontWeight: FontWeight.w500,
     letterSpacing: 0.1,
   ),
-  bodyText1: GoogleFonts.urbanist(
+  bodyText1: GoogleFonts.poppins(
     fontSize: 16,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.5,
   ),
-  bodyText2: GoogleFonts.urbanist(
+  bodyText2: GoogleFonts.poppins(
     fontSize: 14,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.25,
   ),
-  button: GoogleFonts.urbanist(
+  button: GoogleFonts.poppins(
     fontSize: 14,
     fontWeight: FontWeight.w500,
     letterSpacing: 1.25,
   ),
-  caption: GoogleFonts.urbanist(
+  caption: GoogleFonts.poppins(
     fontSize: 12,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.4,
   ),
-  overline: GoogleFonts.urbanist(
+  overline: GoogleFonts.poppins(
     fontSize: 10,
     fontWeight: FontWeight.w400,
     letterSpacing: 1.5,
